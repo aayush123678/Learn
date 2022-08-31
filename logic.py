2c8d015854aa6af874829fab72c296df945ae9e2
Aayush Jha <99559412+aayush123678@users.noreply.github.com>
2022-08-30 22:10:26 -0500

    Update logic.py
    Removes testing preset conditions

--- logic.py
+++ logic.py
@@ -1,18 +1,19 @@
 from random import choice, randrange, shuffle
 import os
-
-from function import rem, succ, written,multiple_choice
+from function import rem, succ, written, multiple_choice
 
 
 # extracts the file information
-with open('test.in', 'r') as full_elements:
+with open('Sets/School/Science/Chem/input.in', 'r') as full_elements:
     full_elements = full_elements.readlines()
 full_elements = rem(full_elements, '\n')
-print(full_elements)
-# removes the extra spaces in the file
+# removes the spaces and shamless self promotion.
 while '' in full_elements:
     full_elements.remove('')
-print(full_elements)
+while 'Upgrade to remove ads' in full_elements:
+    full_elements.remove('Upgrade to remove ads')
+while 'Only $35.99/year' in full_elements:
+    full_elements.remove('Only $35.99/year')
 evens = full_elements[::2]
 odds = full_elements[1::2]
 while len(odds) < 4:
@@ -20,15 +21,13 @@ while len(odds) < 4:
 while len(evens) < 4:
     evens.append(None)
 # Multiple choice or written?
-#questions_type = input('Mutiple choice or written? m/w\n>')
-questions_type = 'm'
+questions_type = input('Mutiple choice or written? m/w\n>')
 # Sets how many times you want to study the set
-#times = int(input('How many times should the set repeat?\n>'))
-times = 1
+times = int(input('How many times should the set repeat?\n>'))
 study_elements = times*full_elements
 os.system('cls')
 if questions_type.lower() == 'w':
     written(study_elements, full_elements)
 elif questions_type.lower() == 'm':
     multiple_choice(study_elements, full_elements, evens, odds)
-
+print('Congratulations!!')
